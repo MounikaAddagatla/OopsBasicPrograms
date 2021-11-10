62dcd0ccdb92761178e2c343a7d3a80aa709efb6
unknown <mounaadagatla@gmail.com>
2021-11-10 06:53:26 +0530

    Add : UC1 added for read json data

---
+++ OopsBasicPrograms/OopsBasicPrograms/Program.cs
@@ -0,0 +1,26 @@
+﻿using OopsBasicPrograms.JsonInventoryModel;
+using System;
+
+namespace OopsBasicPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oops Basic Programs");
+            Console.WriteLine("Choose 1.JSONInventoryDataManagement");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch (options)
+            {
+                case 1:
+                    InventoryManagementMain inventoryMain = new InventoryManagementMain();
+                    inventoryMain.DisplayData(@"D:\OopsBasicPrograms\OopsBasicPrograms\OopsBasicPrograms\JsonInventoryModel\Inventory.json");
+                    break;
+                default:
+                    Console.WriteLine("Choose valid option");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
