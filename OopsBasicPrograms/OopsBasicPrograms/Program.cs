6f3335254cf2c63568e72fba4e27f13f5a1454e0
unknown <mounaadagatla@gmail.com>
2021-11-10 07:06:39 +0530

    Add: Inventory manager forinventory Factory

--- OopsBasicPrograms/OopsBasicPrograms/Program.cs
+++ OopsBasicPrograms/OopsBasicPrograms/Program.cs
@@ -8,13 +8,18 @@ namespace OopsBasicPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Oops Basic Programs");
-            Console.WriteLine("Choose 1.JSONInventoryDataManagement");
+            InventoryManagementMain inventoryMain = new InventoryManagementMain();
+            Console.WriteLine("Choose 1.JSONInventoryDataManagement ReadData \n2 InventoryManager");
             int options = Convert.ToInt32(Console.ReadLine());
             switch (options)
             {
                 case 1:
-                    InventoryManagementMain inventoryMain = new InventoryManagementMain();
-                    inventoryMain.DisplayData(@"D:\OopsBasicPrograms\OopsBasicPrograms\OopsBasicPrograms\JsonInventoryModel\Inventory.json");
+                    inventoryMain.ReadData(@"D:\OopsBasicPrograms\OopsBasicPrograms\OopsBasicPrograms\JsonInventoryModel\Inventory.json");
+                    break;
+                case 2:
+                    inventoryMain.ReadData(@"D:\OopsBasicPrograms\OopsBasicPrograms\OopsBasicPrograms\JsonInventoryModel\Inventory.json");
+                    Console.WriteLine("Enter Rice or Wheat or Pulses ");
+                    inventoryMain.Display(Console.ReadLine());
                     break;
                 default:
                     Console.WriteLine("Choose valid option");
